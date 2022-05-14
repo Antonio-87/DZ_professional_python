ef08deae873e2a1a1e497208fb91d47d8fc19a2b
Antonio-87 <davydenko.87@list.ru>
2022-05-14 20:44:36 +0300

    Add_correct_iter

--- DZ_Iterators_Generators_Yield/iter_class.py
+++ DZ_Iterators_Generators_Yield/iter_class.py
@@ -18,20 +18,24 @@ class FlatIterator:
 
 class FlatIterator_any:
     def __init__(self, nested_list):
-        self.flat_list = sum(nested_list, [])
+        self.flat_list = self.flatten(nested_list)
         
           
     def __iter__(self):
         self.start = -1
         self.end = len(self.flat_list)
         return self
-      
+
+    
+    def flatten(self, nested_list):
+        if isinstance(nested_list, list):   
+            return sum(map(self.flatten, nested_list), [])
+        else:
+            return [nested_list]  
+
 
     def __next__(self):
         self.start += 1
         if self.start == self.end:
             raise StopIteration
-        if isinstance(self.flat_list[self.start], list):
-            for x in self.flat_list[self.start]:
-                return x
         return self.flat_list[self.start]
